1199846ee9f9317b554c697a24f51e45802410e5
pierreviences <imaniyazid@gmail.com>
2024-01-10 14:31:34 +0700

    chore: update sdk v34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.jajanyuk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jajanyuk"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
