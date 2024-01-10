bc4456faa0042dda2f0dc41ebb40ce163e03ca98
pierreviences <imaniyazid@gmail.com>
2024-01-10 14:27:34 +0700

    style: add ui login

---
+++ app/src/main/java/com/example/jajanyuk/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jajanyuk.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jajanyuk.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
