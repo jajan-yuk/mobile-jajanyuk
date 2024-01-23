fab85656e62a1b1027a746f0ad19310340892366
pierreviences <imaniyazid@gmail.com>
2024-01-24 06:17:04 +0700

    style: add ui pembayaran

---
+++ app/src/main/java/com/example/jajanyuk/ui/pembeli/pembayaran_activity.kt
@@ -0,0 +1,12 @@
+package com.example.jajanyuk.ui.pembeli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jajanyuk.R
+
+class pembayaran_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembayaran)
+    }
+}
